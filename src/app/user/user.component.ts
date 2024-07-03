dc9f22da4e65ac322088c66f75a37a45d1d31dd3
Shubham Samarth <shubhams@heaptrace.net>
2024-07-03 17:19:25 +0530

    signals

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -8,15 +8,18 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   standalone: true,
   imports: [],
   templateUrl: './user.component.html',
-  styleUrl: './user.component.css',
+  styleUrls: ['./user.component.css'],
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
-  get imgPath() {
-    return 'assets/users/' + this.selectedUser.avatar;
-  }
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+
+  imgPath = computed(() => {
+    return 'assets/users/' + this.selectedUser().avatar;
+  });
 
   onSelectUser() {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
     console.log('clicked');
   }
 }
