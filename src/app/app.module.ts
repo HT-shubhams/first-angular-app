c161d0a38cb9430ce112ebab19bfa4ef203aa0cc
shubhamsamarth <shubhams@heaptrace.net>
2024-07-09 15:08:21 +0530

    Migrating All Components To Use Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { TasksComponent } from './tasks/tasks.component';
+import { CardComponent } from './shared/card/card.component';
+import { TaskComponent } from './tasks/task/task.component';
+import { NewTaskComponent } from './tasks/new-task/new-task.component';
 
 @NgModule({
-  declarations: [AppComponent], // for non-standalone components
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    UserComponent,
+    CardComponent,
+    TasksComponent,
+    TaskComponent,
+    NewTaskComponent,
+  ], // for non-standalone components
   bootstrap: [AppComponent],
-  imports: [BrowserModule, HeaderComponent, UserComponent, TasksComponent], // for standalone components
+  imports: [BrowserModule, FormsModule], // for standalone components
 })
 export class AppModule {}
